1e927a8dfdbf29cea87aebba3ee7933ba8b721d2
ali mizbani <alimizbani888@gmail.com>
2023-06-18 10:45:50 +0430

    making navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import {
   Box,
   IconButton,
   Container,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import Logo from "../../assets/Logo.png";
 import { FiMenu } from "react-icons/fi";
@@ -14,9 +15,12 @@ import { FiMenu } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 import Navpages from "./Navpages";
+import { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const isDesktop = useBreakpointValue({ base: false, lg: true });
+  const [isToggled, setIsToggled] = useState(false);
   return (
     <Box as="section" pb={{ base: "12", md: "24" }}>
       <Box as="nav" bg="bg.surface" boxShadow="sm">
@@ -29,14 +33,52 @@ const Navbar = () => {
                 <Button>SGIN IN</Button>
               </Link>
             </Flex>
-          ) : (
+          ) : !isToggled ? (
             <IconButton
+              onClick={() => setIsToggled(!isToggled)}
               variant="tertiary"
               icon={<FiMenu fontSize="1.25rem" />}
               aria-label="Open Menu"
-            >
-              <Link to="/">Home</Link>
-            </IconButton>
+            />
+          ) : (
+            <SimpleGrid>
+              <Box textAlign="center" width="100px" height="200px" bg="white">
+                <Button marginLeft={10} boxSize="25px">
+                  <XMarkIcon
+                    color="black"
+                    onClick={() => setIsToggled(!isToggled)}
+                  />
+                </Button>
+                <Button
+                  marginBottom={3}
+                  _hover={{ color: "pink.500" }}
+                  variant="link"
+                >
+                  <Link to="/">Home</Link>
+                </Button>
+                <Button
+                  marginBottom={3}
+                  _hover={{ color: "pink.500" }}
+                  variant="link"
+                >
+                  <Link to="/contactus">ContactUs</Link>
+                </Button>
+                <Button
+                  marginBottom={3}
+                  _hover={{ color: "pink.500" }}
+                  variant="link"
+                >
+                  <Link to="/benefits">Benefits</Link>
+                </Button>
+                <Button
+                  marginBottom={3}
+                  _hover={{ color: "pink.500" }}
+                  variant="link"
+                >
+                  <Link to="/ourclasses">OurClasses</Link>
+                </Button>
+              </Box>
+            </SimpleGrid>
           )}
         </HStack>
       </Box>
